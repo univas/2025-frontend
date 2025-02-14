b690de318a699d6c592b9e15375e95dea37dbde8
unknown <Administrador@LABF530128.fuvs.br>
2025-02-13 22:11:29 -0300

    aula 02 + exercícios

--- index.js
+++ index.js
@@ -52,6 +52,102 @@ function soma(a, b){
     return a + b
 }
 
-subtracao = (a,b) => {
+subtracao = (a,b) => { // arrow function
     return a - b
 }
+
+function divisao(a, b){
+    if(b == 0){
+        throw new Error("Não pode fazer divisão por zero.")
+    }
+
+    return a/b
+}
+
+function executar(valor1, valor2, operacao){ // função callback
+    return operacao(valor1, valor2)
+}
+
+executar(10, 5, subtracao)
+
+// console.log(executar(10, 0, divisao))
+
+
+function potencia(a, b){
+    let resultado = 1
+    for(let i = 1; i <= b; i++){
+        resultado = resultado * a
+    }
+
+    return resultado
+}
+
+
+
+
+// vetores
+
+// int numeros[10];
+var numeros = [10,20,30,40,50]
+
+console.log(numeros)
+
+for(let i = 0; i < numeros.length; i++){
+    console.log(numeros[i])
+}
+
+numeros.forEach(function(valor){
+    console.log(valor)
+})
+
+const numeros2 = numeros.map(function(numero){
+    return numero*numero
+})
+
+console.log(numeros, numeros2)
+
+
+
+const alunos = [
+    "Marcos",
+    "Caio",
+    "Carlos",
+    "Joao",
+    "Fabio"
+]
+
+alunos.forEach(function (aluno){
+    console.log(aluno)
+})
+
+// contar quantos alunos começam com a letra c
+let contador = 0
+
+alunos.forEach(function(aluno){
+    if(aluno.startsWith("C")){
+        contador++
+    }
+})
+
+console.log("Alunos com C:", contador)
+
+// map => sempre vai retornar um novo array com
+// a mesma quantidade de elementos do array original
+const alunosTransformados = alunos.map(function(aluno){
+    return aluno.toUpperCase()
+})
+
+console.log(alunos, alunosTransformados)
+
+
+
+
+/**
+ * 1. Montar um array de números e exibir na tela apenas os números pares usando foreach
+ * 
+ * 2. Montar um array de números e exibir na tela apenas os números primos usando foreach
+ * 
+ * 3. Montar um array de nomes e criar um novo array contendo todas as letras minusculas usando map
+ * 
+ * 4. Montar um array de nomes e criar um novo array contendo todas as letras "a" trocadas por "b"
+ */
