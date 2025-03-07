f7ea09f1d0ca8b5ebf09e1805701b99e4cf8c263
unknown <Administrador@LABF530128.fuvs.br>
2025-03-06 22:00:36 -0300

    aula sobre formulários

---
+++ formularios/index.js
@@ -0,0 +1,6 @@
+const nomeInput = document.getElementById("nome")
+const nomeLabel = document.getElementById("labelNome")
+
+nomeLabel.addEventListener("click", function(){
+    nomeInput.value = "Marcos"
+})
