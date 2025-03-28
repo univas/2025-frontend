578921e2e644d9955bcdb359713ef6b3a40b0d59
unknown <Administrador@LABF530128.fuvs.br>
2025-03-27 21:29:58 -0300

    atividade json-server api

---
+++ api/atividade.js
@@ -0,0 +1,20 @@
+
+
+
+// Instalar a biblioteca json-server
+// npm install -g json-server => CMD
+
+// Criar um arquivo .json com um objeto de usuários
+
+// acessar a pasta pelo CMD e iniciar a API
+// json-server --watch {nome_do_arquivo.json} --port 3000
+
+// Abrir a extensão Thunder Client (instalar se precisar)
+// Criar todas as requisições abaixo:
+// GET All
+// GET by ID
+// POST
+// PUT
+// DELETE
+
+// github.com/univas/2025-frontend
